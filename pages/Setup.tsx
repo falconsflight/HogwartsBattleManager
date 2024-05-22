4c2d872f01493d55963f1c225c89e15ce5604fdb
jacob.falcon <jefalcon444@gmail.com>
2024-05-21 22:32:15 -0500

    Configure setup page with basic styling and new dropdown component

--- pages/Setup.tsx
+++ pages/Setup.tsx
@@ -4,24 +4,39 @@ import {
   View,
   Button,
   TouchableOpacity,
-  ToastAndroid
+  StyleSheet
 } from 'react-native';
+import DropdownComponent from '../components/DropdownComponent';
 
 function SetupPage({ navigation }) {
     const [checkBoxSelected, setCheckBoxSelected] = useState([]);
-    const [radioSelected, setRadioSelected] = useState(1);
     const charactersJson = require('../Characters.json');
+    const years = [
+        {label: '1', value: 1},
+        {label: '2', value: 2},
+        {label: '3', value: 3},
+        {label: '4', value: 4},
+        {label: '5', value: 5},
+        {label: '6', value: 6},
+        {label: '7', value: 7},
+    ];
 
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Choose your characters...</Text>
-        <CheckBox props={charactersJson.characters}></CheckBox>
-        <Text>Choose your year...</Text>
-        <RadioButton></RadioButton>
-        <Button
-          title="Play"
-          onPress={() => navigation.popToTop()}
-        />
+      <View style={{ flex: 1, alignItems: 'stretch'}}>
+        <View style={{ flex: 2}}>
+            <Text style={styles.text}>Select Hogwarts Year</Text>
+            <DropdownComponent data={years} dropdownLabel="Year"/>
+        </View>
+        <View style={{ flex: 2}}>
+            <Text style={styles.text}>Select Characters</Text>
+            <CheckBox props={charactersJson.characters}></CheckBox>
+        </View>
+        <View style={{ flex: 4, marginTop: 5}}>
+            <Button
+            title="Play"
+            onPress={() => navigation.popToTop()}
+            />
+        </View>
         {
             /*
                 Test code for when I was learning navigation...
@@ -40,43 +55,6 @@ function SetupPage({ navigation }) {
       </View>
     );
 
-    function RadioButton(){ 
-        const years = [1, 2, 3, 4, 5, 6, 7];
-        return (
-          years.map((val) => {
-            return (
-                <View
-            style={{flex: 1, flexWrap: "wrap", flexDirection: "row", justifyContent: 'center', alignItems: 'center'}}>
-              <TouchableOpacity key={val} onPress={() => {setRadioSelected(val)}}>
-                <View style={{
-                  height: 20,
-                  width: 20,
-                  borderRadius: 12,
-                  borderWidth: 2,
-                  borderColor: '#000',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  display: 'inline'
-                }}>
-                  {
-                    val == radioSelected ?
-                      <View style={{
-                        height: 10,
-                        width: 10,
-                        borderRadius: 6,
-                        backgroundColor: '#000',
-                      }} />
-                      : null
-                  }
-                </View>
-              </TouchableOpacity>
-              <Text key={'text'+val} style={{paddingLeft: 5}}>{val}</Text>
-              </View>
-            )
-          })
-        );
-      }
-
       function ToggleCheckBox(id : number){
         if(checkBoxSelected.includes(id)){
             setCheckBoxSelected(checkBoxSelected.filter((item) => item != id));
@@ -84,28 +62,17 @@ function SetupPage({ navigation }) {
             setCheckBoxSelected([...checkBoxSelected, id]);
         }
       }
-
-      function toast(message){
-        ToastAndroid.showWithGravity(message, ToastAndroid.SHORT, ToastAndroid.TOP);
-      };
     
       function CheckBox(props){ 
         /*
             Test code for determining types and data for objects and arrays. 
             console.log("low type of variable is:"+typeof(props.props.characters));
             console.log("low data of variable is:"+JSON.stringify(props.props.characters));
-            
-            let test = ' ';
-            props.props.characters.map((val) => {
-            test += ' ' + val.name;
-            });
-            toast(test);
         */
         return (
           props.props.map((val) => {
             return (
-                <View
-            style={{flex: 1, flexWrap: "wrap", flexDirection: "row", justifyContent: 'center', alignItems: 'center'}}>
+            <View style={styles.checkboxView}>
               <TouchableOpacity key={val.id} onPress={() => {ToggleCheckBox(val.id)}}>
                 <View style={{
                   height: 20,
@@ -126,12 +93,34 @@ function SetupPage({ navigation }) {
                   }
                 </View>
               </TouchableOpacity>
-              <Text style={{paddingLeft: 5}}>{val.name}</Text>
-              </View>
+              <Text style={styles.checkboxText}>{val.name}</Text>
+            </View>
             )
           })
         );
       }
 }
+const styles = StyleSheet.create({
+    text: {
+        fontFamily: "Georgia, serif",
+        fontSize:20,
+        fontWeight: 'bold',
+        paddingLeft: 5
+    },
+    checkboxText: {
+        fontFamily: "Georgia, serif",
+        fontSize: 16,
+        paddingLeft: 5
+    },
+    checkboxView: {
+        flex: 1,
+        flexWrap: "wrap",
+        flexDirection: "row",
+        justifyContent: 'left',
+        alignItems: 'center',
+        padding: 5,
+        backgroundColor: 'white'
+    }
+  });
 
 export default SetupPage;

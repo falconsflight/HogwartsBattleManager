e2dc4324acc10b57587d164f2b70943f2a09047c
jacob.falcon <jefalcon444@gmail.com>
2024-06-05 16:05:33 -0500

    Adding colors constants for app

---
+++ lib/Colors.tsx
@@ -0,0 +1,7 @@
+export const White = "#ffffff";
+export const Transparent = "#FFFFFF00";
+export const Gray = "#a8a7a5";
+export const Spell = "#d14136"
+export const Item = "#c9d406";
+export const Ally = "#5353ed";
+export const CardBack = "#592a03";

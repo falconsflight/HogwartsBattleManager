2a7470966d74aada6a944e4a437a5053b1344933
Jacob Falcon <jefalcon444@gmail.com>
2024-06-08 17:16:46 -0500

    play around with some colors for legibility

--- lib/Colors.tsx
+++ lib/Colors.tsx
@@ -1,7 +1,8 @@
 export const White = "#ffffff";
 export const Transparent = "#FFFFFF00";
 export const Gray = "#a8a7a5";
-export const Spell = "#d14136"
-export const Item = "#c9d406";
-export const Ally = "#5353ed";
+export const Spell = "#fc655d"
+export const Item = "#edfc5d";
+export const Ally = "#5d63fc";
 export const CardBack = "#592a03";
+export const CardDescription = "#e9f5c4";

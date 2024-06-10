ea5f9993760fd8302d65e649fc215c735577acbc
Jacob Falcon <jefalcon444@gmail.com>
2024-06-10 07:54:54 -0500

    adding scrollview to gallery; centering cards; showing cost on card

--- pages/Gallery.tsx
+++ pages/Gallery.tsx
@@ -4,7 +4,8 @@ import {
   View,
   Button,
   TouchableOpacity,
-  StyleSheet
+  StyleSheet,
+  ScrollView
 } from 'react-native';
 import DropdownComponent from '../components/DropdownComponent';
 import { createDeck, shuffleCards } from '../lib/UtilityFunctions';
@@ -16,83 +17,83 @@ function GalleryPage({ navigation }) {
     const [cardsInGallery, setCardsInGallery] = useState([]);
 
     return (
-      <View style={{ flex: 1, alignItems: 'stretch'}}>
-        <View style={{ flex: 4, marginTop: 5}}>
-            <Button
-            title="Back to Home"
-            onPress={() => {
-                navigation.navigate('Home', {});
-            }}
-            />
-            <Button
-            title="Harry's Cards"
-            onPress={() => {
-                getDeckOfCards(Cards.starterCards[1])
-            }}
-            />
-            <Button
-            title="Hermione's Cards"
-            onPress={() => {
-                getDeckOfCards(Cards.starterCards[2])
-            }}
-            />
-            <Button
-            title="Ron's Cards"
-            onPress={() => {
-                getDeckOfCards(Cards.starterCards[3])
-            }}
-            />
-            <Button
-            title="Neville's Cards"
-            onPress={() => {
-                getDeckOfCards(Cards.starterCards[4])
-            }}
-            />
-            <Button
-            title="Game 1 Cards"
-            onPress={() => {
-                getDeckOfCards(Cards.hogwartsCards[1])
-            }}
-            />
-            <Button
-            title="Game 2 Cards"
-            onPress={() => {
-                getDeckOfCards(Cards.hogwartsCards[2])
-            }}
-            />
-            <Button
-            title="Game 3 Cards"
-            onPress={() => {
-                getDeckOfCards(Cards.hogwartsCards[3])
-            }}
-            />
-            <Button
-            title="Game 4 Cards"
-            onPress={() => {
-                getDeckOfCards(Cards.hogwartsCards[4])
-            }}
-            />
-            <Button
-            title="Game 5 Cards"
-            onPress={() => {
-                getDeckOfCards(Cards.hogwartsCards[5])
-            }}
-            />
-            <Button
-            title="Game 6 Cards"
-            onPress={() => {
-                getDeckOfCards(Cards.hogwartsCards[6])
-            }}
-            />
-            <Button
-            title="Game 7 Cards"
-            onPress={() => {
-                getDeckOfCards(Cards.hogwartsCards[7])
-            }}
-            />
-            {cardsInGallery.map((card) => CreateCard(card))}
-        </View>
+    <ScrollView contentInsetAdjustmentBehavior="automatic">
+      <View style={{ flex: 1, alignItems: 'center'}}>
+        <Button
+        title="Back to Home"
+        onPress={() => {
+            navigation.navigate('Home', {});
+        }}
+        />
+        <Button
+        title="Harry's Cards"
+        onPress={() => {
+            getDeckOfCards(Cards.starterCards[1])
+        }}
+        />
+        <Button
+        title="Hermione's Cards"
+        onPress={() => {
+            getDeckOfCards(Cards.starterCards[2])
+        }}
+        />
+        <Button
+        title="Ron's Cards"
+        onPress={() => {
+            getDeckOfCards(Cards.starterCards[3])
+        }}
+        />
+        <Button
+        title="Neville's Cards"
+        onPress={() => {
+            getDeckOfCards(Cards.starterCards[4])
+        }}
+        />
+        <Button
+        title="Game 1 Cards"
+        onPress={() => {
+            getDeckOfCards(Cards.hogwartsCards[1])
+        }}
+        />
+        <Button
+        title="Game 2 Cards"
+        onPress={() => {
+            getDeckOfCards(Cards.hogwartsCards[2])
+        }}
+        />
+        <Button
+        title="Game 3 Cards"
+        onPress={() => {
+            getDeckOfCards(Cards.hogwartsCards[3])
+        }}
+        />
+        <Button
+        title="Game 4 Cards"
+        onPress={() => {
+            getDeckOfCards(Cards.hogwartsCards[4])
+        }}
+        />
+        <Button
+        title="Game 5 Cards"
+        onPress={() => {
+            getDeckOfCards(Cards.hogwartsCards[5])
+        }}
+        />
+        <Button
+        title="Game 6 Cards"
+        onPress={() => {
+            getDeckOfCards(Cards.hogwartsCards[6])
+        }}
+        />
+        <Button
+        title="Game 7 Cards"
+        onPress={() => {
+            getDeckOfCards(Cards.hogwartsCards[7])
+        }}
+        />
+        {cardsInGallery.map((card) => CreateCard(card))}
       </View>
+    </ScrollView>
     );
 
     function getDeckOfCards(cardSet: CardProps[]){
@@ -103,7 +104,7 @@ function GalleryPage({ navigation }) {
     function CreateCard(card: CardProps){//might need to add cost to CardProps eventually
         return (
           <>
-          <Card id={card.id} name={card.name} description={card.description} type={card.type} cost={0} discardFn={nullFunction}></Card>
+          <Card id={card.id} name={card.name} description={card.description} type={card.type} cost={card.cost} discardFn={nullFunction}></Card>
           </>
         )
     }

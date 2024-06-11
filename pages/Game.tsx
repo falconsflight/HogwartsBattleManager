4d651f2d52c27a9eec5567cbe1514b94be5cf084
Jacob Falcon <jefalcon444@gmail.com>
2024-06-10 23:02:41 -0500

    Add modal for getting player choice on which deck to place a selected shelf item; Create store in new way

--- pages/Game.tsx
+++ pages/Game.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import {
+  Alert,
   Button,
+  Modal,
+  Pressable,
   ScrollView,
+  StyleSheet,
   Text,
   View
 } from 'react-native';
@@ -18,7 +22,12 @@ const GamePage = ({ route, navigation}) => {
     const charactersData = charactersJson.characters;
     const [ currentPlayer, setCurrentPlayer ] = useState(0);
     const players = SetupPlayers(characters);
-    const store = CreateStore(year);
+    const shelfLimit = 6;
+    const [storeShelf, setStoreShelf] = useState([]);
+    const [storeStock, setStoreStock] = useState(CreateStore(year));
+    const [modalVisible, setModalVisible] = useState(false);
+    let acquiredCard = "";
+    storeStock.map((card) => console.log(card.name));
     
     return (
       <ScrollView contentInsetAdjustmentBehavior="automatic">
@@ -28,16 +37,50 @@ const GamePage = ({ route, navigation}) => {
             <Text>Your selected characters are: {JSON.stringify(characters)}</Text>
             <Text>Your selected year is: {year}</Text>
           </View>
-          {store}
-          <Text>{GetCharacterName(characters[currentPlayer]-1)}'s turn</Text>
+          <Store drawPile={storeStock} shelf={storeShelf} drawFn={addToShelf} acquireFn={acquireCard}></Store>
+          <Text>{GetCurrentPlayerName()}'s turn</Text>
           <Button
             title="End Turn"
             onPress={() => {EndTurn()}}
           />
             {players}
         </View>
+
+        <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Please choose where to place {acquiredCard} for {GetCurrentPlayerName()}</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Draw pile</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Hand</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Discard pile</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
       </ScrollView>
     );
+
+    function GetCurrentPlayerName(){
+      return GetCharacterName(characters[currentPlayer]-1);
+    }
 
     function GetCharacterNames(ids: number[]){
       let names = "";
@@ -58,6 +101,12 @@ const GamePage = ({ route, navigation}) => {
       }
     }
 
+    function addToShelf(){
+      if(storeShelf.length < shelfLimit){
+          setStoreShelf([...storeShelf, storeStock.pop()])
+      }
+    }
+
     function CreateStore(year: number){
       let i = 1;
       let deck: CardProps[][] = [];
@@ -66,11 +115,14 @@ const GamePage = ({ route, navigation}) => {
       }
       let finalDeck = shuffleCards(deck.flat());
       
-      return <Store cards={finalDeck} acquireFn={acquireCard}></Store>
+      return finalDeck;
     }
 
-    function acquireCard(){
-
+    function acquireCard(id: string){
+      acquiredCard = storeShelf.filter((card: CardProps) => card.id == id)[0];
+      //need to add acquiredCard to player's hand, draw pile, or discard pile
+      setModalVisible(true);
+      setStoreShelf(storeShelf.filter((card: CardProps) => card.id != id));
     }
     
     function SetupPlayers(characters: number[]){
@@ -81,5 +133,50 @@ const GamePage = ({ route, navigation}) => {
       return <><Player character={charactersData[characterId-1]}></Player></>
     }
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    margin: 3
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
 
 export default GamePage;

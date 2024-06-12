52aa16aa8d31e1f67a39a510ce4d54a37e5023e3
jacob.falcon <jefalcon444@gmail.com>
2024-06-12 14:07:03 -0500

    move hand and card rendering to their own components; remove unused code

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,39 +1,17 @@
-import React, {useState} from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, ToastAndroid, Button } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native'
 import * as Colors from '../lib/Colors'
 import Card from './Card';
-import { CardProps } from '../models/CardProps';
 import { PlayerProps } from '../models/PlayerProps';
 import { CardData } from '../models/CardData';
 
 const Player = (props: PlayerProps) => {
     const name = props.character.name;
 
-    const renderHand = () =>{
-        if (props.hand.length > 0) {
-            return (
-                <View style={[
-                    styles.handView, 
-                    { 
-                        flex: 1, 
-                        flexWrap: "wrap", 
-                        flexDirection: "row", 
-                        padding: 10, 
-                        justifyContent: 'center', 
-                        alignItems: 'center'
-                    }]
-                }>
-                {props.hand.map((card) => CreateCard(card))}
-                </View>
-            );
-          }
-          return null;
-    }
-
     return(
         <View style={[styles.playerBoard,{ flex: 1, flexWrap: "wrap", flexDirection: "column", padding: 10, justifyContent: 'center', alignItems: 'center'}]}>
             <View style={{flex:1}}>
-            <Text>{name}</Text>
+            <Text style={{fontSize:25}}>{name}</Text>
             </View>
             <View style={{flex:1}}>
             <Button
@@ -41,21 +19,47 @@ const Player = (props: PlayerProps) => {
                 onPress={() => {props.drawFn(props.character.id)}}
             />
             </View>
-            {renderHand()}
+            {renderHand(props.hand, props.character.id, props.discardFn)}
             <Button
                 title={"Discards (" + props.discardPile.length + ")"}
                 onPress={() => {/* add logic here for modal to show discards in order*/}}
             />
         </View>
     );
-      
-      function CreateCard(card: CardData){
-          return (
-            <>
-            <Card id={card.id} playerId={props.character.id} name={card.name} description={card.description} type={card.type} cost={card.cost} discardFn={props.discardFn}></Card>
-            </>
-          )
+}
+
+const renderHand = (hand: Readonly<CardData[]>, playerId: Readonly<number>, discardFn: Readonly<Function>) =>{
+    if (hand.length > 0) {
+        return (
+            <View style={[
+                styles.handView, 
+                { 
+                    flex: 1, 
+                    flexWrap: "wrap", 
+                    flexDirection: "row", 
+                    padding: 10, 
+                    justifyContent: 'center', 
+                    alignItems: 'center'
+                }]
+            }>
+            {hand.map((card) => renderCard(card, playerId, discardFn))}
+            </View>
+        );
       }
+      return null;
+}
+
+const renderCard = (card: Readonly<CardData>, playerId: Readonly<number>, discardFn: Readonly<Function>) =>{
+    return (
+        <Card
+        id={card.id}
+        playerId={playerId}
+        name={card.name}
+        description={card.description}
+        type={card.type}
+        cost={card.cost}
+        discardFn={discardFn}/>
+      )
 }
 
 const styles = StyleSheet.create({

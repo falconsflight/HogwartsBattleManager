f692d971f816744fba57756888e266c1e474a8de
Jacob Falcon <jefalcon444@gmail.com>
2024-06-15 15:41:31 -0500

    adding health and influence meters to players

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, Button, Pressable } from 'react-native'
 import * as Colors from '../lib/Colors'
 import Card from './Card';
 import { PlayerProps } from '../models/PlayerProps';
@@ -8,12 +8,16 @@ import { CardData } from '../models/CardData';
 const Player = (props: PlayerProps) => {
     const [showDiscards, setShowDiscards] = useState(false);
     const [showTopCard, setShowTopCard] = useState(false);
+    const [health, setHealth] = useState(10);
+    const [influence, setInfluence] = useState(0);
     const name = props.character.name;
 
     return(
         <View style={[styles.playerBoard,
         { 
-            backgroundColor: props.isActive ? Colors.Gray : Colors.InactivePlayer,
+            backgroundColor: 
+                health == 0 ? Colors.PlayerStunned : 
+                props.isActive ? Colors.Gray : Colors.LightGray,
             flex: 1, 
             flexWrap: "wrap", 
             flexDirection: "column", 
@@ -22,6 +26,10 @@ const Player = (props: PlayerProps) => {
             alignItems: 'center'
         }]}>
             {renderName(name, props.isActive)}
+            <View style={{flexDirection: 'row'}}>
+            {renderHealth(health, setHealth)}
+            {renderInfluence(influence, setInfluence)}
+            </View>
             <View style={{flex:1, marginBottom: 5}}>
             <Button
                 title={((getShowOrHideText(showTopCard)))+" top card"}
@@ -50,6 +58,92 @@ function getShowOrHideText(flag: boolean){
 
 function nullFunction(){}
 
+const renderHealth = (health: number, updateHealth: Function) => {
+    let stunnedText = health > 0 ? null : <Text>Stunned!</Text>;
+    return (
+        <View style={{flex: 1,
+            flexDirection: "column", 
+            padding: 10, 
+            justifyContent: 'center', 
+            alignItems: 'center'}}>
+            <View style={{flex: 1, flexDirection: "row"}}>
+                    <View style={{marginRight: 3}}>
+                        <Button
+                        title={"+ 1"}
+                        onPress={() => {if(health < 10)updateHealth(health+1)}}
+                        />
+                    </View>
+                    <View style={{marginRight: 3}}>
+                        <Button
+                        title={"+ 2"}
+                        onPress={() => {
+                            if(health < 9){updateHealth(health+2)}else{updateHealth(10)}
+                            }}
+                        />
+                    </View>
+                    <View style={{marginRight: 3}}>
+                        <Button
+                            title={"- 1"}
+                            onPress={() => {if(health > 0)updateHealth(health-1)}}
+                        />
+                    </View>
+                    <Button
+                        title={"- 2"}
+                        onPress={() => {
+                            if(health > 1){updateHealth(health-2)}else{updateHealth(0)}
+                            }}
+                    />
+            </View>
+            <View style={{marginTop: 3}}>
+                <Button
+                    title={"Reset"}
+                    onPress={() => {updateHealth(10)}}
+                />
+            </View>
+            <Text style={styles.healthText}>Health: {health}</Text>
+            {stunnedText}
+        </View>
+    );
+}
+
+const renderInfluence = (influence: number, updateInfluence: Function) =>{
+    return (
+        <View style={{flex: 1,
+            flexDirection: "column", 
+            padding: 10, 
+            justifyContent: 'center', 
+            alignItems: 'center'}}>
+            <View style={{flex: 1, flexDirection: "row"}}>
+                <View style={{marginRight: 3}}>
+                    <Button
+                    title={"+ 1"}
+                    onPress={() => {updateInfluence(influence+1)}}
+                    />
+                </View>
+                <View style={{marginRight: 3}}>
+                    <Button
+                    title={"+ 2"}
+                    onPress={() => {updateInfluence(influence+2)}}
+                    />
+                </View>
+                <View style={{marginRight: 3}}>
+                <Button
+                    title={"- 1"}
+                    onPress={() => {if(influence > 0)updateInfluence(influence-1)}}
+                />
+                </View>
+            </View>
+            <View style={{marginTop: 3}}>
+                <Button
+                    title={"Reset"}
+                    onPress={() => {updateInfluence(0)}}
+                />
+            </View>
+            <Text style={styles.healthText}>Influence: {influence}</Text>
+        </View>
+    );
+}
+
 const renderTopCard = (showTopCard: boolean, deck: Readonly<CardData[]>, playerId: Readonly<number>) => {
     if(showTopCard){
         if(deck.length > 0){
@@ -63,7 +157,7 @@ const renderName = (name: string, isActive: boolean) =>{
     let display = isActive ? name + "'s Turn" : name;
     return(
         <View style={{flex:1}}>
-            <Text style={{fontSize:25}}>{display}</Text>
+            <Text style={styles.nameText}>{display}</Text>
         </View>
     );
 }
@@ -120,8 +214,17 @@ const styles = StyleSheet.create({
         backgroundColor: Colors.White
     },
     nameText:{
+        fontSize: 25,
         textAlign: 'center',
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        color: Colors.LightBlack
+    },
+    healthText:{
+        fontSize: 15,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: Colors.LightBlack,
+        padding: 5
     }
 });
 

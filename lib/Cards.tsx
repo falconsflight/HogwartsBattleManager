0149ab68c81384464bf31d1732ad1ddb6a604db1
Jacob Falcon <jefalcon444@gmail.com>
2024-06-15 07:05:36 -0500

    Add dark arts cards to Cards constant for simpler access

--- lib/Cards.tsx
+++ lib/Cards.tsx
@@ -1,6 +1,7 @@
 const Cards = {
     starterCards : require('../lib/StarterCards.json'),
-    hogwartsCards : require('../lib/HogwartsCards.json')
+    hogwartsCards : require('../lib/HogwartsCards.json'),
+    darkArtsCards : require('../lib/DarkArtsCards.json')
 }
 
 export default Cards;

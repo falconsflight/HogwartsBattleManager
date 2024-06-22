36c539483ef963ccddd2e0104c7b0a18eae66ba5
jacob.falcon <jefalcon444@gmail.com>
2024-06-21 21:22:47 -0500

    creating full game stylesheet

---
+++ lib/GameStyle.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import * as Colors from '../lib/Colors'
+
+export const gameStyle = StyleSheet.create({
+    tokenText:{
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: Colors.LightBlack,
+        padding: 5
+    }
+});
